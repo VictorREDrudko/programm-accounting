861c2ed706f3c51a52eb9ea2357bddfb222b1a61
Victor RED <viktor_RED_rudko@mail.ru>
2024-05-12 19:11:49 +0300

    refactor: change Header

--- src/layout/header/Header_Styles.ts
+++ src/layout/header/Header_Styles.ts
@@ -14,6 +14,11 @@ const Header = styled.header`
 
 const Logo = styled.img`
   width: 150px;
+  transition: ${Theme.animation.transition};
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `
 
 const Nav = styled.nav`
@@ -22,24 +27,57 @@ const Nav = styled.nav`
   }
 
   & img {
-    width: 40px;
+    width: 30px;
     position: absolute;
-    top: -10px;
-    left: -50px;
+    top: -5px;
+    left: -40px;
+    transition: ${Theme.animation.transition};;
     
+    &:hover {
+      transform: scale(1.1);
+    }
   }
 
   & a {
     display: inline-block;
     text-transform: uppercase;
     position: relative;
+    transition: ${Theme.animation.transition};;
+  }
+
+  & a::after {
+    content: "";
+    display: inline-block;
+    height: 1px;
+    width: 100%;
+    background-color: ${Theme.colors.fontHover};
+    transform: scale(0);
+    position: absolute;
+    top: 25px;
+    left: 0;
+    transition: ${Theme.animation.transition};;
+  }
+
+  & a:hover {
+    color: ${Theme.colors.fontHover};
+
+    &::after {
+      transform: scale(1);
+    }
   }
 `
 
 const LoginContainer = styled.div`
   & img {
-    width: 60px;
+    width: 40px;
+    transition: ${Theme.animation.transition};
+
+    &:hover {
+      transform: scale(1.2);
+    }
   }
+
+
 `
 
 export const S = {
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,6 +1,11 @@
 export const Theme = {
   colors: {
     mainBg: "#365421",
-    font: "#7AFB23"
-  }
+    font: "#FFFFFF",
+    fontHover: "#7AFB23"
+  },
+
+  animation: {
+    transition: "0.5s ease-in-out"
+  },
 }
